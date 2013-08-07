ec532d39fb59baaf2aa12f72318c520599d19762
Armin Noroozi <arminelec@gmail.com>
2013-08-07 16:31:03 +0200

    render with quality

--- src/main/java/com/ribomation/droidAtScreen/gui/DeviceFrame.java
+++ src/main/java/com/ribomation/droidAtScreen/gui/DeviceFrame.java
@@ -303,6 +303,11 @@ public class DeviceFrame extends JFrame implements Comparable<DeviceFrame> {
 		protected void paintComponent(Graphics g) {
 			if (image != null && g instanceof Graphics2D) {
 				Graphics2D g2 = (Graphics2D) g;
+				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+				g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+				g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
 				AffineTransform TX = new AffineTransform();
 				BufferedImage bufImg = image.toBufferedImage();
 
