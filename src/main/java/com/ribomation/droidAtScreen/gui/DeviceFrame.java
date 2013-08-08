2889efda3826b72cd0519e00b0c048404f6af90b
Armin Noroozi <arminelec@gmail.com>
2013-08-08 11:52:19 +0200

    more performance!

--- src/main/java/com/ribomation/droidAtScreen/gui/DeviceFrame.java
+++ src/main/java/com/ribomation/droidAtScreen/gui/DeviceFrame.java
@@ -366,9 +366,9 @@ public class DeviceFrame extends JFrame implements Comparable<DeviceFrame> {
 		protected void paintComponent(Graphics g) {
 			if (image != null && g instanceof Graphics2D) {
 				Graphics2D g2 = (Graphics2D) g;
-				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-				g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-				g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+				g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
+				g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_SPEED);
 				g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
 
 				AffineTransform TX = new AffineTransform();
